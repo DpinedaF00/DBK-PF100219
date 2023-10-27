74baaf99717ff69bba85eebf6a15a5c2d1cc0ef8
unknown <UFG@CC1-51.ufg.local>
2023-10-26 20:11:55 -0600

    creacion de endpoint para nuevo pais y para obtener todos los paises

---
+++ app/Http/Controllers/PaisController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pais;
+
+class PaisController extends Controller
+{
+    //
+
+    public function crearPais(Request $request){
+        $Pais = new Pais(); 
+        $Pais->nombre_pais = $request->nombre;
+        $Pais->save();
+
+        return response()->json([
+            "success"=>true,
+            "message"=>"pais ingresado",
+            "data"=>"Se creo el registro con el id: ".$Pais->id,
+            "cantidad"=> 1
+        ]);
+    }
+
+    public function obtener(){
+        $Pais = new Pais(); 
+        $datos = $Pais::All();
+
+        return response()->json([
+            "success"=>true,
+            "message"=>"aqui estan todos los datos",
+            "data"=>$datos,
+            "cantidad"=> 1
+        ]);
+    }
+}
